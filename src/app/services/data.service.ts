abe760e428523c4847df5b7e2179563d43ec314b
antoinethebault <antoine.thebau@gmail.com>
2020-04-28 10:11:25 +0200

    cache

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { matricules } from '../mock/matricules.mock';
 import { Collegues } from '../mock/collegues.mock';
 import { Collegue } from '../models/Collegue';
-import { HttpClient, HttpResponse} from "@angular/common/http";
-import {environment} from '../../environments/environment';
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { environment } from '../../environments/environment';
 //import {AppComponent} from '../app.component';
 import { Observable, interval, Subject } from 'rxjs';
 
@@ -13,28 +13,24 @@ import { Observable, interval, Subject } from 'rxjs';
 export class DataService {
 
   constructor(private _http: HttpClient) { }
-  
 
-  listeMatricules:string[];
-  collegues:Collegue[];
+
+  listeMatricules: string[];
+  collegues: Collegue[];
   URL_BACKEND = environment.backendUrl;
   private collegueEnCours = new Subject<Collegue>();
+  cacheCollegues = {};
 
   //retourne la liste des matricules correspondant au nom recherche
   rechercherParNom(nom: string): string[] {
+    this.cacheCollegues = {};
     this.listeMatricules = [];
-    this.collegues = Collegues.collegues;
-
-    /*this.collegues.forEach(collegue => {
-      if (collegue.nom == nom){
-        this.listeMatricules.push(collegue.matricule);
-      }
-    });*/
-    this._http.get(this.URL_BACKEND+"collegues?nom="+nom) .subscribe((data: string[]) => {
-      for (let i=0; i<data.length; i++){
+    
+    this._http.get(this.URL_BACKEND + "collegues?nom=" + nom).subscribe((data: string[]) => {
+      for (let i = 0; i < data.length; i++) {
         this.listeMatricules.push(data[i]);
       }
-    }, (error:any) => {
+    }, (error: any) => {
       console.log("erreur lors de la requete de recherche par nom");
     });
     return this.listeMatricules;
@@ -44,14 +40,18 @@ export class DataService {
   recupererCollegueCourant(): Collegue {
     return Collegues.collegues[0];
   }
-  
-  recupererCollegueMatricule(matricule:string) {
-    this._http.get(this.URL_BACKEND+"collegues/"+matricule) .subscribe((data: Collegue) => {
-      //this.app.afficherCollegue(data);
-      this.collegueEnCours.next(data);
-    }, (error:any) => {
-      console.log("erreur lors de la requete de recherche par matricule");
-    });
+
+  recupererCollegueMatricule(matricule: string) {
+    if (this.cacheCollegues[matricule]) {
+      this.collegueEnCours.next(this.cacheCollegues[matricule]);
+    } else {
+      this._http.get(this.URL_BACKEND + "collegues/" + matricule).subscribe((data: Collegue) => {
+        this.cacheCollegues[matricule] = data;
+        this.collegueEnCours.next(data);
+      }, (error: any) => {
+        console.log("erreur lors de la requete de recherche par matricule");
+      });
+    }
   }
 
   abonnementCollegueEnCours(): Observable<Collegue> {
